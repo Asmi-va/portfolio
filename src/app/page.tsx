4fb2ff6d819331babe074167c4a0fa865e6924cc
Asmi Vashista <asmivashisth@gmail.com>
2025-07-03 12:16:29 +0530

    Remove unused Text import for clean build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion";
-import { Text } from '@react-three/drei';
 import React from 'react';
 import Link from "next/link";
 import { Typewriter } from 'react-simple-typewriter';
